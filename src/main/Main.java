10c3bfc6f431d126c15d87e15408808301cafb51
Sasha <45601549+Bendersnatch@users.noreply.github.com>
2025-02-03 19:53:35 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,11 @@ public class Main {
         window.setResizable(false);
         window.setTitle("DD-Adventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
